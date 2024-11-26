091daf57fb377315416c60f054bca13ce911eef7
tomas23zx <joaotomasfreitas@gmail.com>
2024-11-26 19:23:02 +0000

    Update Unidades.java

--- src/poo/Unidades.java
+++ src/poo/Unidades.java
@@ -1,10 +1,16 @@
-package poo;
-public abstract class Unidades()
-{
-private int posicaox;
-private int posicaoy;
 
+class Unidades {
+    private String letra;
+    private int linha;
+    private int coluna;
 
+    public Unidades(String letra, int linha, int coluna) {
+        this.letra = letra;
+        this.linha = linha;
+        this.coluna = coluna;
+    }
 
-
+    public String getLetra() {
+        return letra;
+    }
 }
