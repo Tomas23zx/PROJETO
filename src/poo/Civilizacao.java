a74535b00c8b457a583af5864b2713ed96748225
tomas23zx <joaotomasfreitas@gmail.com>
2024-12-08 17:00:04 +0000

    Create Civilizacao.java

---
+++ src/poo/Civilizacao.java
@@ -0,0 +1,61 @@
+package poo;
+
+
+import java.util.ArrayList;
+
+public class Civilizacao {
+private ArrayList<Cidade> citys;
+private String nome;
+public Civilizacao(String nome){
+
+
+
+this.nome=nome;
+citys=new ArrayList<>();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
