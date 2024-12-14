f15b2126507a18e4ef4fbdcad799a83a222d456b
tomas23zx <joaotomasfreitas@gmail.com>
2024-12-14 18:03:26 +0000

    .

--- Documents/GitHub/PROJETO/src/poo/Menu.java
+++ Documents/GitHub/PROJETO/src/poo/Menu.java
@@ -1,8 +1,8 @@
 package poo;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Menu {
     private Scanner scanner = new Scanner(System.in);
